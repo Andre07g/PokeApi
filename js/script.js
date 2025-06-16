14192beb6e63ea42a50a3ebeef8e102056ff91d3
Andre07G <eleal087@gmail.com>
2025-06-15 20:02:09 -0500

    feat: :sparkles: Created cards page
    The cards page and its functions was created
    
    by@Andres

--- js/script.js
+++ js/script.js
@@ -32,38 +32,63 @@ function funcionesPC(pagina) {
         botonGif.addEventListener("click", () => {
 
             if (posicionGif < 4) { botonGif.setAttribute("src", listaGif[posicionGif]); posicionGif += 1 }
-            else { botonGif.setAttribute("src", listaGif[0]); posicionGif = 1 }})
+            else { botonGif.setAttribute("src", listaGif[0]); posicionGif = 1 }
+        })
         botonFondo.addEventListener("click", () => {
             botonFondo.setAttribute("src", "../assets/variedsources/fondopulsado.png");
             botonFondo.style.top = "17.5vh";
             setTimeout(() => {
                 botonFondo.setAttribute("src", "../assets/variedsources/fondonormal.png");
                 botonFondo.style.top = "15vh";
-            }, 200); 
+            }, 200);
             if (posicionWal < 10) { fondo.style.backgroundImage = `url(${listaFondos[posicionWal]})`; posicionWal += 1 }
-            else { fondo.style.backgroundImage = `url(${listaFondos[0]})`; posicionWal = 1 }})
-        botonPokedex.addEventListener("click",()=>{
+            else { fondo.style.backgroundImage = `url(${listaFondos[0]})`; posicionWal = 1 }
+        })
+        botonPokedex.addEventListener("click", () => {
             botonPokedex.setAttribute("src", "../assets/variedsources/pokedexpulsado.png");
             botonPokedex.style.top = "42vh";
             setTimeout(() => {
                 botonPokedex.setAttribute("src", "../assets/variedsources/pokedexnormal.png");
                 botonPokedex.style.top = "40vh";
             }, 200);
-            setTimeout(()=>{window.open("../html/pokedex.html","_self")},300)
+            setTimeout(() => { window.open("../html/pokedex.html", "_self") }, 300)
         });
-        botonCartas.addEventListener("click",()=>{
+        botonCartas.addEventListener("click", () => {
             botonCartas.setAttribute("src", "../assets/variedsources/cartaspulsado.png");
             botonCartas.style.top = "67vh";
             setTimeout(() => {
                 botonCartas.setAttribute("src", "../assets/variedsources/cartasnormal.png");
                 botonCartas.style.top = "65vh";
-            }, 200); 
-            setTimeout(()=>{window.open("../html/cards.html","_self")},300)
+            }, 200);
+            setTimeout(() => { window.open("../html/cards.html", "_self") }, 300)
         })
 
-    } else if (pagina === "pokeapi.html") {
-        console.log("PC: funciones para pokeapi");
-        // Funciones para otra página
+    } else if (pagina === "cards.html") {
+        const cards = document.querySelectorAll('.carta');
+
+        cards.forEach(card => {
+            card.addEventListener('mousemove', (e) => {
+                const rect = card.getBoundingClientRect();
+                const x = e.clientX - rect.left;
+                const y = e.offsetY;
+                const centerX = rect.width / 2;
+                const centerY = rect.height / 2;
+
+                const rotateX = ((y - centerY) / centerY) * 10;
+                const rotateY = ((x - centerX) / centerX) * 10;
+
+                card.style.transform = `rotateX(${-rotateX}deg) rotateY(${rotateY}deg) scale(1.1)`;
+            });
+
+            card.addEventListener('mouseleave', () => {
+                card.style.transform = 'rotateX(0deg) rotateY(0deg) scale(1)';
+            });
+        });
+
+
+        document.querySelector('.carta-contenedor').addEventListener('mouseleave', () => {
+            card.style.transform = `rotateX(0deg) rotateY(0deg) scale(1)`;
+        });
     }
 }
 
